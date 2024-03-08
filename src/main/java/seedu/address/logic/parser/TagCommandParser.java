4faa387d17bd9c3fd1757a90a5ab68ccbebe8a3c
yyccbb <yu.chenbo@u.nus.edu>
2024-03-08 15:42:49 +0800

    Attempt to add a new command

---
+++ src/main/java/seedu/address/logic/parser/TagCommandParser.java
@@ -0,0 +1,12 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+public class TagCommandParser implements Parser {
+
+    @Override
+    public Command parse(String userInput) throws ParseException {
+        return null;
+    }
+}
