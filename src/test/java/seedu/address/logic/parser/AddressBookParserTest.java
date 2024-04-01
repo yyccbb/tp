dba05e905714b1b9103fd08609dcb7a738fea3af
Wang Xinrong <wangxinrong8@gmail.com>
2024-04-01 17:47:03 +0800

    Add DeletetagCommand Test for AddressBookParserTest

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -14,16 +14,8 @@ import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.ClearCommand;
-import seedu.address.logic.commands.DeleteCommand;
-import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.*;
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
-import seedu.address.logic.commands.ExitCommand;
-import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.HelpCommand;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.FieldContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
@@ -95,6 +87,11 @@ public class AddressBookParserTest {
     @Test
     public void parseCommand_mark() throws Exception {
         assertTrue(parser.parseCommand(MarkCommand.SAMPLE_COMMAND) instanceof MarkCommand);
+    }
+
+    @Test
+    public void parseCommand_deletetag() throws Exception {
+        assertTrue(parser.parseCommand(DeletetagCommand.SAMPLE_COMMAND) instanceof DeletetagCommand);
     }
 
     @Test
