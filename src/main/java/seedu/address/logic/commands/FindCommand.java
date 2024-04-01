c2bd416cd8f8d4b393026f3eb20caad837eee773
yyccbb <yu.chenbo@u.nus.edu>
2024-04-02 00:24:18 +0800

    Bug fix: display person in singular form when only 1 found

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -33,6 +33,11 @@ public class FindCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.persistentUpdateFilteredList(this.predicates);
+
+        int filteredListSize = model.getFilteredPersonList().size();
+        if (filteredListSize == 1) {
+            return new CommandResult(Messages.MESSAGE_ONE_PERSON_LISTED_OVERVIEW);
+        }
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
     }
